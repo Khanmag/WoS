8bbd964298686e10c7f40d7c98354bdc83486822
Khanmag <Khanmag329@gmail.com>
2023-01-08 09:43:14 +0300

    gh pages setting

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import store from './redux/redux-store'
 import {Provider} from "react-redux";
 
@@ -21,11 +21,11 @@ window.store = store
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
     root.render(
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL} >
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
